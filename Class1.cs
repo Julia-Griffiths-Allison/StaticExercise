24b77d4c284817c700afeacfb21488b8efa20d24
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-09-26 14:46:31 -0400

    first commit, finished farenheight and celcius convertion

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var answer = (fahrenheit - 32) / 1.8;
+
+            return answer;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using StaticExercise;
+
+var celsius = TempConverter.FahrenheitToCelsius(86);
+var fahrenheit = TempConverter.CelsiusToFahrenheit(30);
+
+Console.WriteLine($"{fahrenheit} degrees farenheit would be equal to");
+Console.WriteLine($"{celsius} degrees celcius.");
